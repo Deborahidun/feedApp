6582accc9940871d3a0507cc1ecfd2f55ced842e
Deborah <debbyidun@gmail.com>
2024-11-05 14:14:20 -0500

    UserService update

--- src/main/java/com/bptn/feedApp/service/UserService.java
+++ src/main/java/com/bptn/feedApp/service/UserService.java
@@ -1,28 +1,32 @@
 package com.bptn.feedApp.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.bptn.feedApp.jdbc.UserBean;
-import com.bptn.feedApp.jdbc.UserDao;
+import com.bptn.feedApp.jpa.User;
+import com.bptn.feedApp.repository.UserRepository;
 
 @Service
 public class UserService {
 
 	@Autowired
-	UserDao userDao;
+	UserRepository userRepository;
 
-	public List<UserBean> listUsers() {
-		return this.userDao.listUsers();
+	// Method to list all users
+	public List<User> listUsers() {
+		return this.userRepository.findAll();
 	}
 
-	public UserBean findByUsername(String username) {
-		return this.userDao.findByUsername(username);
+	// Method to find a user by username
+	public Optional<User> findByUsername(String username) {
+		return this.userRepository.findByUsername(username);
 	}
 
-	public void createUser(UserBean user) {
-		this.userDao.createUser(user);
+	// Method to create a new user
+	public void createUser(User user) {
+		this.userRepository.save(user);
 	}
 }
